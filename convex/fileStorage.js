4aa4c7532d5b49b989775474654d9046a12b0327
Phuc Nguyen <phucnguyen0331@gmail.com>
2025-01-19 02:23:12 -0800

    add save file storge and connect to db

--- convex/fileStorage.js
+++ convex/fileStorage.js
@@ -1,5 +1,24 @@
 import { mutation } from "./_generated/server";
+import { v } from "convex/values";
 
 export const generateUploadUrl = mutation(async (ctx) => {
   return await ctx.storage.generateUploadUrl();
 });
+
+export const AddFileEntryToDb = mutation({
+    args:{
+        fileId:v.string(),
+        storageId:v.string(),
+        fileName:v.string(),
+        createBy:v.string(),
+    },
+    handler:async(ctx,args)=>{
+        const result =await ctx.db.insert('pdfFiles',{
+            fileId:args.fileId,
+            fileName:args.fileName,
+            storageId:args.storageId,
+            createBy:args.createBy,
+        })
+        return 'Inserted'
+    }
+})
